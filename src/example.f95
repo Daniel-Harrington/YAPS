a599314742f5f0a246db28d487d482693f51aac5
Lucinho21 <nlucianetti21@hotmail.com>
2024-11-09 19:29:01 -0500

    added changes

--- src/example.f95
+++ src/example.f95
@@ -1,7 +1,9 @@
-subroutine gravity(a,  b)
+subroutine gravity(a,  b, c)
     integer, intent(in) :: a
     integer, intent(out) ::  b
+    integer, intent(out) :: c
     b = a**2
+    c = b**2 
     
 end subroutine gravity
 
